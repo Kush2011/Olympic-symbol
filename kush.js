eba04288c04536b7bddaaa193649b6b629748078
Kush2011 <74670716+Kush2011@users.noreply.github.com>
2021-03-17 13:48:18 +0530

    Add files via upload

---
+++ kush.js
@@ -0,0 +1,7 @@
+canvas = document.getElementById("myCanvas");
+ctx = canvas.getContext("2d");
+ctx.beginpath();
+ctx.strokeStyle = "red";
+ctx.linewidth = 1;
+ctx.rect(150, 143, 430, 200);
+ctx.strokeStyle();
